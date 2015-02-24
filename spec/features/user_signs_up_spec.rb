df33450b8682e4881db26791af905e478aa44bb8
john ko <johnsko89@gmail.com>
2015-02-24 18:58:54 -0500

    old spec folder

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -13,13 +13,22 @@ feature 'user registers', %Q{
   #   an error message
 
   scenario 'provide valid registration information' do
+    user = FactoryGirl.create(:user)
+
     visit new_user_registration_path
 
-    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Email', with: "jognas@gmail.com"
+    fill_in 'First name', with: user.first_name
+    fill_in 'Last name', with: user.last_name
+    select user.birthday.strftime("%d"), from: "user_birthday_3i"
+    select I18n.translate("date.month_names")[user.birthday.month], from: "user_birthday_2i"
+    select user.birthday.strftime("%Y"), from: "user_birthday_1i"
+    fill_in 'Zip code', with: user.zip_code
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
 
     click_button 'Sign up'
+    save_and_open_page
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(page).to have_content('Sign Out')
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,6 +3,10 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name 'John'
+    last_name 'Ko'
+    birthday { Date.today - 19.years }
+    zip_code '07650'
     password 'password'
     password_confirmation 'password'
   end
